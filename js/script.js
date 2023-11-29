3761b670b347fb682cd57f40d516915a8b14664a
nicomanunta <nic.manun@gmail.com>
2023-11-29 19:03:43 +0100

    mi ero scordato il bottone, bonus finito

--- js/script.js
+++ js/script.js
@@ -1,19 +1,30 @@
 
-
-
-
-function createCell(num){
-    let element = document.createElement("div")
-    element.classList.add("square")
-    element.innerText = num
-
-    return element
-}
-
-let grid = document.getElementById("grid")
-
-for(let i = 0; i<100; i++){
+function createGrid(){
+    document.getElementById("grid").innerHTML = "";
+    function createCell(num){
+        let element = document.createElement("div")
+        element.classList.add("square")
+        element.innerText = num
+        
+        return element
+    }
     
-    let square = createCell(i+1)
-    grid.appendChild(square)
+    for(let i = 0; i<100; i++){
+        
+        let square = createCell(i+1)
+        square.addEventListener("click", function(){
+            this.classList.toggle("clicked")
+            console.log("Hai cliccato la cella numero" +" "+ parseInt(i+1))
+        })
+        grid.appendChild(square)
+    
+    }
+    
 }
+
+
+
+
+document.getElementById("play").addEventListener("click", function(){
+    createGrid()
+})
